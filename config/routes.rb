7a0515bde20909b8c0a76583ac10594cf6b8250e
KD33 <Walshk94@gmail.com>
2018-04-09 00:35:36 +0000

    Added Sections

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#index'
+  resources :courses, only: [:index, :show]
   namespace :instructor do
-    resources :courses, only: [:new, :create, :show]
+    resources :courses, only: [:new, :create, :show] do
+      resources :sections, only: [:new, :create]
+    end
   end
   resources :courses, only: [:index, :show]
 end
