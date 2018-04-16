560c48c1c4d67ea5b31a0195d38df81fe59b932e
KD33 <Walshk94@gmail.com>
2018-04-16 09:16:33 +0000

    working video player

--- app/uploaders/video_uploader.rb
+++ app/uploaders/video_uploader.rb
@@ -1,7 +1,7 @@
-class ThumbnailUploader < CarrierWave::Uploader::Base
+class VideoUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  include CarrierWave::MiniMagick
+  # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   #storage :file
@@ -22,7 +22,6 @@ class ThumbnailUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process resize_to_fill: [800, 350]
   # process scale: [200, 300]
   #
   # def scale(width, height)
