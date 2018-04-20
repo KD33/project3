32b6499aab5112e9affefac9cc00ac2119cf775e
KD33 <Walshk94@gmail.com>
2018-04-20 09:10:06 +0000

    Client side drag & drop

---
+++ app/controllers/enrollments_controller.rb
@@ -0,0 +1,36 @@
+class EnrollmentsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    if current_course.premium?
+      current_user.enrollments.create(course: current_course)
+      # Amount in cents
+      @amount = (current_course.cost * 100).to_i
+
+      customer = Stripe::Customer.create(
+        email: params[:stripeEmail],
+        source: params[:stripeToken]
+      )
+
+      charge = Stripe::Charge.create(
+        customer: customer.id,
+        amount: @amount,
+        description: 'Flixter Premium Content',
+        currency: 'usd'
+      )
+
+      current_user.enrolllments.create(course: current_course)
+      redirect_to course_path(current_course)
+
+    rescue Stripe::CardError => e
+      flash[:error] = e.message
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def current_course
+    current_course = Course.find(id=params[:course_id])
+  end
+end
--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,7 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_enrollment_for_lessons, only: [:show]
+
   def show
 
   end
@@ -9,6 +12,12 @@ helper_method :current_lesson
 
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
+  end
+
+  def require_enrollment_for_lessons
+    if !current_user.enrolled_courses.include?(current_lesson.section.course)
+      redirect_to root_path, alert: 'You Must Enroll In A Course Before Viewing Lessons'
+    end
   end
 
 end
