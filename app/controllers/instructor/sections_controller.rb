f92216266668c5e6b7a8d812f8a02d8a9cba2d67
KD33 <Walshk94@gmail.com>
2018-04-21 22:39:34 +0000

    Drag & Drop Sections

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,6 +1,7 @@
 class Instructor::SectionsController < ApplicationController
   before_action :authenticate_user!
-  before_action :require_authorized_for_current_course
+  before_action :require_authorized_for_current_course, only: [:new, :create]
+  before_action :require_authorized_for_current_section, only: [:update]
 
   def new
     @section = Section.new
@@ -12,10 +13,20 @@ class Instructor::SectionsController < ApplicationController
     redirect_to instructor_course_path(current_course)
   end
 
+  def update
+    current_section.update_attributes(section_params)
+  end
+
   private
 
   def require_authorized_for_current_course
     if current_course.user != current_user
+      render plain: "Unauthorized", status: :unauthorized
+    end
+  end
+
+  def require_authorized_for_current_section
+    if current_section.course.user != current_user
       render plain: "Unauthorized", status: :unauthorized
     end
   end
@@ -26,8 +37,12 @@ class Instructor::SectionsController < ApplicationController
     @current_course ||= Course.find(params[:course_id])
   end
 
+  def current_section
+    @current_section ||= Section.find(params[:id])
+  end
+
   def section_params
-    params.require(:section).permit(:title)
+    params.require(:section).permit(:title, :row_order_position)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,14 @@ Rails.application.routes.draw do
   resources :courses, only: [:index, :show] do
     resources :enrollments, only: :create
   end
+
   resources :lessons, only: [:show]
   namespace :instructor do
     resources :lessons, only: [:update]
-    resources :sections, only: [] do
+    resources :sections, only: [:update] do
       resources :lessons, only: [:new, :create]
     end
+
     resources :courses, only: [:new, :create, :show] do
       resources :sections, only: [:new, :create]
       end
